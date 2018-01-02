b7c5f0f636dc1758384eacec4af7743571ded732
Dries Hooghe <dries@dasmedia.be>
2018-01-02 18:55:59 +0100

    add installation script + general config boilerplates

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# TODO: dynamically load paths from path file and add them to the system path variable
+
+# Load the environment variables
+# TODO: do the same for general use variables in variables
---
+++ installer.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+### Set directory to repo path
+BASEDIR="$(pwd)"
+
+### Check requirements
+
+# ZSH
+if [[ ! $(which zsh) ]]; then
+    echo "ZSH must be installed"
+    exit 1
+fi
+
+# VIM
+if [[ ! $(which vim) ]]; then
+    echo "VIM must be installed"
+    exit 1
+fi
+
+# GIT
+if [[ ! $(which git) ]]; then
+    echo "GIT must be installed"
+    exit 1
+fi
+
+# .ENV
+if [[ ! -f ./.env ]]; then
+    echo ".env file not found!"
+    echo "run \"cp .env.default .env\""
+    exit 1
+fi
+
+### Make symlinks
+
+# ZSH
+ln -s ${BASEDIR}/zshrc $HOME/.zshrc
+if [ $? -eq 0 ]; then
+    /usr/bin/printf "\xE2\x9C\x94 Created ZSH symlink"
+else
+    echo "Could not create ZSH symlink! Aborting..."
+fi
+
+# VIM
+ln -s ${BASEDIR}/vimrc $HOME/.vimrc
+if [ $? -eq 0 ]; then
+    /usr/bin/printf "\xE2\x9C\x94 Created VIMRC symlink"
+else
+    echo "Could not create VIMRC symlink! Aborting..."
+    rm $HOME/.zshrc
+    exit 1
+fi
+ln -s ${BASEDIR}/vim/ $HOME/.vim
+if [ $? -eq 0 ]; then
+    /usr/bin/printf "\xE2\x9C\x94 Created VIM symlink"
+else
+    echo "Could not create VIM symlink! Aborting..."
+    rm $HOME/.zshrc
+    rm $HOME/.vimrc
+    exit 1
+fi
+
+# GIT
+ln -s ${BASEDIR}/gitconfig $HOME/.gitconfig
+if [ $? -eq 0 ]; then
+    /usr/bin/printf "\xE2\x9C\x94 Created GIT symlink"
+else
+    echo "Could not create GIT symlink! Aborting..."
+    rm $HOME/.zshrc
+    rm $HOME/.vimrc
+    rm -R $HOME/.vim
+fi
