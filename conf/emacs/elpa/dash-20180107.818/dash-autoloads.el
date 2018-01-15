3d910f75f34f3cb54ac3f613ef9eb2d900b999c7
Dries Hooghe <dries@dasmedia.be>
2018-01-15 23:22:53 +0100

    add monokai theme to emacs editor

---
+++ conf/emacs/elpa/dash-20180107.818/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dash.el") (23133 9084 731928 739000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
---
+++ conf/emacs/init.el
@@ -0,0 +1,20 @@
+;; skip start screen on launching emacs
+(setq inhibit-startup-message t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+;; set theme
+(load-theme 'monokai t)
+
