cb971bdcba03159e86c10ad58a594660ac730965
Dries Hooghe <dries@dasmedia.be>
2018-01-17 18:43:42 +0100

    enable evil mode in emacs + set emacs alias

--- conf/emacs/init.el
+++ conf/emacs/init.el
@@ -15,9 +15,30 @@
 (use-package try
   :ensure t)
 
+;;requirements
+(require 'evil)
+
 ;; set theme
 (load-theme 'monokai t)
 
 ;; enable the line-numbers-bar and add a space
 (global-linum-mode 1)
 (setq linum-format "%3d \u2502 ")
+
+;; enable evil mode
+(evil-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (evil which-key use-package try solarized-theme monokai-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
