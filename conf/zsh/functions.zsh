26c6b1295b0d42be9c43c912f3ffde7294d5a388
Dries Hooghe <dries@dasmedia.be>
2018-02-13 10:51:53 +0100

    add zsh function to contact remote server

--- conf/zsh/functions.zsh
+++ conf/zsh/functions.zsh
@@ -22,3 +22,12 @@ function myip() {
   ifconfig en1 | grep 'inet ' | sed -e 's/:/ /' | awk '{print "en1 (IPv4): " $2 " " $3 " " $4 " " $5 " " $6}'
   ifconfig en1 | grep 'inet6 ' | sed -e 's/ / /' | awk '{print "en1 (IPv6): " $2 " " $3 " " $4 " " $5 " " $6}'
 }
+
+#-------------------------------
+#          GOTO
+#-------------------------------
+
+function goto(){
+        echo "Trying to reach ${1}@${workserver}"
+        ssh ${1}@${workserver}
+}
