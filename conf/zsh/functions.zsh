26124e09cf1e125e8887fcfa458566febc89fa1f
Dries Hooghe <dries@dasmedia.be>
2018-02-13 11:08:59 +0100

    modify goto function to use optional second parameter

--- conf/zsh/functions.zsh
+++ conf/zsh/functions.zsh
@@ -24,10 +24,17 @@ function myip() {
 }
 
 #-------------------------------
-#          GOTO
+#          GO TO REMOTE
 #-------------------------------
 
 function goto(){
-        echo "Trying to reach ${1}@${workserver}"
-        ssh ${1}@${workserver}
+        if [[ "$#" -eq 2 ]]; then
+                echo "Trying to reach ${1}@${2}"
+                ssh ${1}@${2}
+        elif [[ "$#" -eq 1 ]]; then
+                echo "Trying to reach ${1}@${workserver}"
+                ssh ${1}@${workserver}
+        else
+                echo "Wrong amount of arguments supplied"
+        fi
 }
