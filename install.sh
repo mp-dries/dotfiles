fe7bab3a27e500ce1e24cd592913beda638f2a93
Dries Hooghe <dries.hooghe@gmail.com>
2018-03-04 15:00:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-# Set the project directory
-# If you wish to change this directory, also alter the declaration in /local/zsh/configs/pre/env.sh
-$DOTFILES=$HOME/.tomes
-
 # Clone the project
-git clone https://github.com/drieshooghe/tomes.git $DOTFILES
+git clone https://github.com/drieshooghe/tomes.git $HOME/.tomes
 
 # Source variables and functions required for installer
 for f in "$DOTFILES/local/zsh/configs/pre/"*; do
