3098353292dbedc5b1dafe49383a60af0032d27a
Dries Hooghe <dries.hooghe@gmail.com>
2018-03-17 11:16:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Clone the project to the current directory
 DOTFILE_DIR="$( cd "$(dirname "$0")" ; pwd -P )/dotfiles"
 git clone https://github.com/drieshooghe/tomes.git $DOTFILE_DIR
+echo "export DOTFILES=\"$DOTFILE_DIR\"" >> $DOTFILE_DIR/local/zsh/configs/pre/env.sh
 
 # Source variables and functions required for installer
 for f in "$DOTFILE_DIR/local/zsh/configs/pre/"*; do
