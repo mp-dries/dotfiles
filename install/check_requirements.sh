a4ea3a5f74b29c6665c7ff15d66a2b3e792449e3
Dries Hooghe <dries.hooghe@gmail.com>
2018-03-17 12:24:17 +0100

    Exit installer script if a check fails

--- install/check_requirements.sh
+++ install/check_requirements.sh
@@ -5,29 +5,35 @@
 # Check if the specified dotfiles directoryname is not $HOME/dotfiles
 if [ "$DOTFILES" == "$HOME/dotfiles" ]; then
   cprint error "Thoughtbot's dotfiles directory is already named dotfiles, specify a different directory"
+  exit 1
 fi
 
 # zsh
 if ! which zsh > /dev/null; then
   cprint error "ZSH  must be installed"
+  exit 1
 fi
 
 # git
 if ! which git > /dev/null; then
   cprint error "GIT must be installed"
+  exit 1
 fi
 
 # vim
 if ! which vim > /dev/null; then
   cprint error "VIM must be installed"
+  exit 1
 fi
 
 # tmux
 if ! which tmux > /dev/null; then
   cprint error "TMUX must be installed"
+  exit 1
 fi
 
 # rcm
 if ! which rcup > /dev/null; then
   cprint error "RCM must be installed (see https://github.com/thoughtbot/rcm)"
+  exit 1
 fi
