8155e782c0a5cbd5c1ae5c328f5d6799655b56e2
Dries Hooghe <dries.hooghe@gmail.com>
2018-06-11 11:42:15 +0200

    Installer script test

--- install.sh
+++ install.sh
@@ -5,9 +5,8 @@ DOTFILE_DIR="$( cd "$(dirname '$0')" ; pwd -P )/dotfiles"
 git clone https://github.com/drieshooghe/dotfiles.git $DOTFILE_DIR
 echo '#!/usr/bin/env/ bash\n\nexport DOTFILES="'$DOTFILE_DIR'"' > $DOTFILE_DIR/local/zsh/configs/pre/dotfile-dir.sh
 
-echo $DOTFILE_DIR
 # Source variables and functions required for installer
-for f in "$DOTFILE_DIR/local/zsh/configs/pre/"*; do
+for f in $DOTFILE_DIR/local/zsh/configs/pre/*; do
   source "$f"
 done
 
